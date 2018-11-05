6714fef6da3e3fa248390e9c26013ffa725c74ee
James Scanlon <jscanlon77@outlook.com>
2018-11-05 13:54:45 +0000

    Committing this to my repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { LandingComponent } from './components/landing/landing.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { BlockchainComponent } from './components/blockchain/blockchain.component';
+import { DisplayComponent } from './components/display/display.component';
+import { addCoinReducer } from './components/blockchain/blockchain.reducer';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { AuthEffects } from './store/effects/auth.effects';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainComponent,
+    DisplayComponent,
+    LandingComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({blockchain: addCoinReducer}),
+    EffectsModule.forRoot([AuthEffects]),
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'log-in', component: LogInComponent },
+      { path: 'sign-up', component: SignUpComponent },
+      { path: '', component: LandingComponent },
+      { path: '**', redirectTo: '/' }
+    ])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
